d6ee83bf32d217b46b89e539f65c4694da5e1f85
ammwangi <elijah.mwangi@kamsware.com>
2024-02-20 12:28:03 +0300

    feat: just play

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,29 @@
+﻿using CityInfo.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    [Route("api/cities")]
+    public class CitiesController: ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<CityDto> GetCities()
+        {
+            var result =   CitiesDataStore.Current.Cities;
+            return Ok(result);
+
+        }
+        [HttpGet("{id}")]
+        public ActionResult<CityDto> GetCities(int id)
+        {
+            var response = CitiesDataStore.Current.Cities.FirstOrDefault(x => x.Id == id);
+            if (response == null)
+            {
+                return NotFound();
+            }
+            return Ok(response);
+
+        }
+    }
+}
---
+++ CityInfo.API/Controllers/PointOfInterestController.cs
@@ -0,0 +1,40 @@
+﻿using CityInfo.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/cities/{cityId}/pointsofinterest")]
+    [ApiController]
+    public class PointOfInterestController: ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<PointOfInterestDto> GetPointsOfInterest(int cityId)
+        { 
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(x => x.Id == cityId);
+            if (city == null )
+            {
+                return NotFound();
+            }
+            var points = city.PointsOfInterest.ToList();
+            return Ok(points);
+        }
+
+        [HttpGet("{pointofinterestid}")]
+        public ActionResult GetPointOfInterest(int cityId, int pointofinterestid)
+        {
+            var city  = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if(city == null)
+            {
+                return NotFound();
+            }
+            var pointOfInterest = city.PointsOfInterest.FirstOrDefault(x => x.Id.Equals(pointofinterestid));
+
+            if (pointOfInterest == null)
+            {
+                return NotFound() ;
+            }
+
+            return Ok(pointOfInterest);
+        }
+    }
+}
