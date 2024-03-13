fd012c3fd88074db3ac313fbb07feef6e9c6ec9d
ammwangi <elijah.mwangi@kamsware.com>
2024-03-13 11:57:14 +0300

    custome securing your apis

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -2,7 +2,9 @@ using CityInfo.API.DbContexts;
 using CityInfo.API.Services;
 using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Serilog;
+using System.Text;
 
 
 Log.Logger = new LoggerConfiguration()
@@ -38,6 +40,21 @@ builder.Services.AddDbContext<CityInfoContext>(dbContextOptions  => dbContextOpt
 
 builder.Services.AddScoped<ICityInfoRepository,CityInfoRepository>();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Authentication:Issuer"],
+            ValidAudience = builder.Configuration["Authentication:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.ASCII.GetBytes(builder.Configuration["Authentication:SecretForKey"]))
+        };
+    }
+    );
 
 var app = builder.Build();
 
@@ -53,6 +70,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
